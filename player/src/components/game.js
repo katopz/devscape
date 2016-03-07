6c1cd839d61c8b23fa92789083c61f2a7fd1614c
Banjerdkit <U6034838@ten.thomsonreuters.com>
2016-03-07 13:26:42 +0700

    working mouse down via scene

--- player/src/components/game.js
+++ player/src/components/game.js
@@ -30,7 +30,7 @@ export default class Game extends Component {
 		let { rotateX=0, rotateY=0 } = this.state;
 		this.downState = { rotateX, rotateY }
 		this.down = coords(e);
-        console.log("mouseDown");
+                console.log("mouseDown");
 		e.preventDefault();
 	}
 	
@@ -48,13 +48,13 @@ export default class Game extends Component {
 	mouseUp() {
 		this.downState = null;
 		this.down = null;
+                console.log("mouseUp");
 	}
     
 	render({}, { zoom=1, rotateX=0, rotateY=0 }) {
 		return (
 			<div id="game">
 				<header>
-					<h1>Renderer Example</h1>
 					<input type="range" value={zoom} min="0" max="10" step="0.00001" onInput={ this.linkState('zoom') } />
 				</header>
 				<main {...this.events}>
@@ -65,7 +65,16 @@ export default class Game extends Component {
 	}
 }
 
+@connect(reduce, bindActions(actions))
 class Scene extends Component {
+        constructor() {
+        console.log("Scene.constructor");
+		super();
+		this.events = {
+			[TOUCH?'onTouchStart':'onMouseDown']: ::this.mouseDown
+		};
+	}
+        
 	shouldComponentUpdate() {
 		return false;
 	}
@@ -73,118 +82,125 @@ class Scene extends Component {
 	componentWillReceiveProps() {
 		if (this.base) this.update();
 	}
-	
+        
+  mouseDown(e) {
+    console.log("*scene.mouseDown");
+    e.preventDefault();
+  }
+
 	//@debounce
-	update() {
-        console.log("update");
-		let { zoom, rotateX, rotateY } = this.props;
-		this.object.rotation.y = rotateX * Math.PI;
-		this.object.rotation.z = - rotateY * Math.PI;
-		this.scene.scale.addScalar( zoom - this.scene.scale.x );
-		this.rerender();
-	}
+  update() {
+    console.log(this.props);
+    let { events, zoom, rotateX, rotateY } = this.props;
+    this.object.rotation.y = rotateX * Math.PI;
+    this.object.rotation.z = - rotateY * Math.PI;
+    this.scene.scale.addScalar(zoom - this.scene.scale.x);
+    this.rerender();
+  }
 
 	componentDidMount() {
 		setTimeout( () => this.setup(), 1);
 	}
 	
-	setup() {
-        console.log("setup");
-		let { width, height } = this.base.getBoundingClientRect();
-		this.renderer = new THREE.WebGLRenderer({ antialias: true });
-		this.renderer.setSize(800*2, 640*2);
-		this.base.appendChild(this.renderer.domElement);
+  setup() {
+    console.log("setup");
+    let { width, height } = this.base.getBoundingClientRect();
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setSize(800 * 2, 640 * 2);
+    this.base.appendChild(this.renderer.domElement);
 
-        this.clock = new THREE.Clock()
-		this.scene = new THREE.Scene();
+    this.clock = new THREE.Clock();
+    this.scene = new THREE.Scene();
         
-		/*
-		this.camera = new THREE.PerspectiveCamera(
-			35,         // FOV
-			800 / 640,  // Aspect
-			0.1,        // Near
-			10000       // Far
-		);
-		this.camera.position.set(-15, 10, 15);
-		this.camera.lookAt(this.scene.position);
-        */
+    /*
+    this.camera = new THREE.PerspectiveCamera(
+            35,         // FOV
+            800 / 640,  // Aspect
+            0.1,        // Near
+            10000       // Far
+    );
+    this.camera.position.set(-15, 10, 15);
+    this.camera.lookAt(this.scene.position);
+    */
         
-        this.camera = new THREE.PerspectiveCamera(
-            40, window.innerWidth / window.innerHeight, 
-            1, 10000
-        );
-        this.camera.position.set(900, 900, 900);
-        this.camera.target = new THREE.Vector3(0, 0, 0);
-        this.camera.lookAt(this.camera.target);
-        //this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
-        //this.controls.damping = 0.2;
-        //this.controls.maxPolarAngle = Math.PI / 2;
-        //this.controls.minDistance = 500;
+    this.camera = new THREE.PerspectiveCamera(
+      40, window.innerWidth / window.innerHeight,
+      1, 10000
+      );
+    this.camera.position.set(900, 900, 900);
+    this.camera.target = new THREE.Vector3(0, 0, 0);
+    this.camera.lookAt(this.camera.target);
+    //this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+    //this.controls.damping = 0.2;
+    //this.controls.maxPolarAngle = Math.PI / 2;
+    //this.controls.minDistance = 500;
 		
-		this.renderObject();
-		this.renderLighting();
-		this.rerender();
-        
-        this.decorate();
-	}
+    this.renderObject();
+    this.renderLighting();
+    this.rerender();
+
+    this.decorate();
+  }
     
-    decorate() {
-        var size = 500, step = 50;
-        new Grid(size, step, this.scene)
-    }
+  decorate() {
+    var size = 500, step = 50;
+    new Grid(size, step, this.scene)
+  }
 	
-	rerender() {
-        let self = this;
-        window.requestAnimationFrame(function () { self.rerender(); });
+  rerender() {
+    let self = this;
+    window.requestAnimationFrame(function () { self.rerender(); });
 
-        let delta = this.clock.getDelta();
+    let delta = this.clock.getDelta();
 
-        this.chickens && this.chickens.forEach(function (model) {
-            model.group.children.forEach(function (mesh) {
-                mesh.updateAnimation(1000 * delta);
-                mesh.translateX(model.speed * delta)
-            });
-        });
-        this.renderer.clear();
-        this.renderer.setViewport(0, 0, window.innerWidth, window.innerHeight);
-		this.renderer.render(this.scene, this.camera);
-	}
+    this.chickens && this.chickens.forEach(function (model) {
+      model.group.children.forEach(function (mesh) {
+        mesh.updateAnimation(1000 * delta);
+        mesh.translateX(model.speed * delta)
+      });
+    });
+    this.renderer.clear();
+    this.renderer.setViewport(0, 0, window.innerWidth, window.innerHeight);
+    this.renderer.render(this.scene, this.camera);
+  }
 	
-	renderObject() {
-        var self = this;
-        self.chickens = [];
-        
-		this.object = new THREE.Mesh(
-			new THREE.BoxGeometry(5, 5, 5), 
-			new THREE.MeshLambertMaterial({ color: 0xFF0000 })
-		);
-		this.scene.add(this.object);
-        
-        this.reference = new LoadModels();
-        this.reference.load().then(function () {
-            new Chicken(0, 0, 15, self.reference, self.scene, self.chickens);
-        });
-	}
+  renderObject() {
+    var self = this;
+    self.chickens = [];
+
+    this.object = new THREE.Mesh(
+      new THREE.BoxGeometry(5, 5, 5),
+      new THREE.MeshLambertMaterial({ color: 0xFF0000 })
+      );
+    this.scene.add(this.object);
+
+    this.reference = new LoadModels();
+    this.reference.load().then(function () {
+      new Chicken(0, 0, 15, self.reference, self.scene, self.chickens);
+    });
+  }
 	
-	renderLighting() {
-		let light = new THREE.PointLight(0xFF0000, 1, 100);
-		light.position.set( 10, 0, 10 );
-		this.scene.add(light);
+  renderLighting() {
+    let light = new THREE.PointLight(0xFF0000, 1, 100);
+    light.position.set(10, 0, 10);
+    this.scene.add(light);
 
-		light = new THREE.PointLight(0xFF0000, 1, 50);
-		light.position.set( -20, 15, 10 );
-		this.scene.add(light);
+    light = new THREE.PointLight(0xFF0000, 1, 50);
+    light.position.set(-20, 15, 10);
+    this.scene.add(light);
 
-		this.renderer.setClearColor(0x222222, 1);
+    this.renderer.setClearColor(0x222222, 1);
+
+    this.ambientLight = new THREE.AmbientLight("#FFFFF3");
+    this.scene.add(this.ambientLight);
+    this.directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    this.directionalLight.position.set(1, 0.75, 0.5).normalize();
+    this.scene.add(this.directionalLight);
+  }
         
-        this.ambientLight = new THREE.AmbientLight("#FFFFF3");
-        this.scene.add(this.ambientLight);
-        this.directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-        this.directionalLight.position.set(1, 0.75, 0.5).normalize();
-        this.scene.add(this.directionalLight);
-	}
-
-	render() {
-		return <div class="scene" />;
-	}
+  render() {
+    return (<main {...this.events } >
+      <div class="scene"/>
+      </main>)
+  }
 }
