bd18040e263a6aa2ecb74959bf7a2dfc96295ce0
Banjerdkit <U6034838@ten.thomsonreuters.com>
2016-04-29 14:17:09 +0700

    More truck

--- player/src/core/ground.js
+++ player/src/core/ground.js
@@ -4,14 +4,14 @@ export default class Ground {
 
     constructor(scene) {
         var _W = 800;
-        var _H = 800;
+        var _H = 240;
 
-        this._buildRoad(scene, 2, -_W, 240, _H * 5, 240, 32);
-        this._buildGrass(scene, -_W, -_H + 240 * .5, _W * 5, _H*2, 32);
-        this._buildGrass(scene, -_W, 240 + 128*.5 + 128, _W*5, 128, 32);
-        
-        this._buildSoid(scene, -_W, 240 + 128*.5 + 128, _W*5, 128, 320);
-        this._buildSoid2(scene, _W + _W/2, -_H - 320*2 - 128, _W*5, 128, 320);
+        this._buildRoad(scene, 2, -_W, _H, _W * 5, _H, 32);
+        this._buildGrass(scene, -_W, -_W + _H * .5, _W * 5, _W * 2, 32);
+        this._buildGrass(scene, -_W, _H + 128 * .5 + 128, _W * 5, 128, 32);
+
+        this._buildSoil(scene, -_W, _H + 128 * .5 + 128, _W * 5, 128, 320);
+        this._buildSoil2(scene, 1200, - _W/2 - 128 - 30, 2000 - 20, 128, 320);
     }
 
     _buildGrass(scene, x, z, w, d, h) {
@@ -23,15 +23,15 @@ export default class Ground {
 
         scene.add(this.mesh);
     }
-    
-    _buildSoid(scene, x, z, w, d, h) {
+
+    _buildSoil(scene, x, z, w, d, h) {
         var geometry = new THREE.BoxGeometry(w, h, d);
         var material = new THREE.MeshPhongMaterial({ color: 0xbf755a }); //, emissive: 0x000000, specular: 0x000000, shininess: 0});
 
         this.mesh = new THREE.Mesh(geometry, material);
         this.mesh.position.set(x, -h / 2 - 0.3 - 32, z);
         //scene.add(this.mesh);
-        
+
         let _SECTION_W = 320;
         var texture = new THREE.TextureLoader().load("textures/soil_decal.png", function(texture) {
             texture.wrapS = THREE.RepeatWrapping;
@@ -40,21 +40,21 @@ export default class Ground {
             var material = new THREE.MeshBasicMaterial({ map: texture });
             var geometry = new THREE.PlaneGeometry(w, _SECTION_W, 1);
             var plane = new THREE.Mesh(geometry, material);
-            plane.position.set(x, -320*0.5 - 0.3 - 32 , z + 128 *0.5);
+            plane.position.set(x, -320 * 0.5 - 0.3 - 32, z + 128 * 0.5);
             //plane.rotation.x = -Math.PI / 2;
             scene.add(plane);
         });
     }
 
-    
-    _buildSoid2(scene, x, z, w, d, h) {
+
+    _buildSoil2(scene, x, z, w, d, h) {
         var geometry = new THREE.BoxGeometry(w, h, d);
         var material = new THREE.MeshPhongMaterial({ color: 0xbf755a }); //, emissive: 0x000000, specular: 0x000000, shininess: 0});
 
         this.mesh = new THREE.Mesh(geometry, material);
         this.mesh.position.set(x, -h / 2 - 0.3 - 32, z);
         //scene.add(this.mesh);
-        
+
         let _SECTION_W = 320;
         var texture = new THREE.TextureLoader().load("textures/soil_decal.png", function(texture) {
             texture.wrapS = THREE.RepeatWrapping;
@@ -63,12 +63,12 @@ export default class Ground {
             var material = new THREE.MeshBasicMaterial({ map: texture });
             var geometry = new THREE.PlaneGeometry(w, _SECTION_W, 1);
             var plane = new THREE.Mesh(geometry, material);
-            plane.position.set(x, -320*0.5 - 0.3 - 32 , z + 128 *0.5);
+            plane.position.set(x, -320 * 0.5 - 0.3 - 32, z + 128 * 0.5);
             plane.rotation.y = Math.PI / 2;
             scene.add(plane);
         });
     }
-    
+
     _buildRoad(scene, lane, x, z, w, d, h) {
         // decal : 0x7b8496
         // road : 0x525763
